f5d50866e11916f9cb7d0dde16f901daae7e3460
Oxynger <141go1@gmail.com>
2022-03-31 22:11:25 +0300

    feat: First layout

--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,47 @@ namespace gimme_oil
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
+            Console.Write("some");
+
             InitializeComponent();
         }
 
+        private void toolStripProgressBar1_Click(object sender, EventArgs e)
+        {
+            Console.Write("some");
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            Console.Write("some");
+
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            Console.Write("some");
+
+
+        }
+
+        private void ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Console.Write("some");
+
+
+        }
+
+        // private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        // {
+        //     this.toolStripMenuItem2.Text = "112";
+        // }
+
+        private void отчетыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // this.toolStripMenuItem2.Text = "112";
+        }
     }
 }
